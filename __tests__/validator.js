490bbc0b407c636d5f8effb22adc57e7e4ecded7
Jon Urry <jon@urry.me>
2019-04-06 14:55:17 +0100

    Validate columns

--- __tests__/validator.js
+++ __tests__/validator.js
@@ -1,5 +1,13 @@
 import Validator from '../src/validator.js';
 
+const invalidColumns = {
+  columns: -5,
+  rows: 5,
+  position: { x: 1, y: 2 },
+  dirt: [{ x: 1, y: 0 }, { x: 2, y: 2 }, { x: 2, y: 3 }],
+  drivingInstructions: ['N', 'N', 'E', 'S', 'E', 'E', 'S', 'W', 'N', 'W', 'W']
+};
+
 const validSample = {
   columns: 5,
   rows: 5,
@@ -9,10 +17,21 @@ const validSample = {
 };
 
 describe('validator', () => {
+  let validator;
+
+  beforeAll(() => {
+    validator = new Validator();
+  });
+
   test('valid input', () => {
-    const validator = new Validator();
     expect(() => {
       validator.check(validSample);
     }).not.toThrow();
+  });
+
+  test('invalid columns', () => {
+    expect(() => {
+      validator.check(invalidColumns);
+    }).toThrow();
   });
 });
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,11 @@
+const checkPositiveNumber = (name, value) => {
+  if (typeof value === 'number' && value < 0) {
+    throw `Validation error, '${name}' must be a positive number. Value passed was ${value}`;
+  }
+};
+
 export default class Validator {
-  check() {
-    console.log('Inside Validator');
+  check(data) {
+    checkPositiveNumber('columns', data.columns);
   }
 }
