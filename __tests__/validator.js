b1f28ad2424aa755b63c1d9814659525ca6b11a1
Jon Urry <jon@urry.me>
2019-04-06 15:39:28 +0100

    Validate types, values and coordinates

--- __tests__/validator.js
+++ __tests__/validator.js
@@ -1,29 +1,5 @@
 import Validator from '../src/validator.js';
 
-const invalidColumns = {
-  columns: -5,
-  rows: 5,
-  position: { x: 1, y: 2 },
-  dirt: [{ x: 1, y: 0 }, { x: 2, y: 2 }, { x: 2, y: 3 }],
-  drivingInstructions: ['N', 'N', 'E', 'S', 'E', 'E', 'S', 'W', 'N', 'W', 'W']
-};
-
-const invalidRows = {
-  columns: 5,
-  rows: -5,
-  position: { x: 1, y: 2 },
-  dirt: [{ x: 1, y: 0 }, { x: 2, y: 2 }, { x: 2, y: 3 }],
-  drivingInstructions: ['N', 'N', 'E', 'S', 'E', 'E', 'S', 'W', 'N', 'W', 'W']
-};
-
-const validSample = {
-  columns: 5,
-  rows: 5,
-  position: { x: 1, y: 2 },
-  dirt: [{ x: 1, y: 0 }, { x: 2, y: 2 }, { x: 2, y: 3 }],
-  drivingInstructions: ['N', 'N', 'E', 'S', 'E', 'E', 'S', 'W', 'N', 'W', 'W']
-};
-
 describe('validator', () => {
   let validator;
 
@@ -32,20 +8,106 @@ describe('validator', () => {
   });
 
   test('valid input', () => {
+    const validSample = {
+      columns: 5,
+      rows: 5,
+      position: { x: 1, y: 2 },
+      dirt: [{ x: 1, y: 0 }, { x: 2, y: 2 }, { x: 2, y: 3 }],
+      drivingInstructions: ['N', 'N', 'E', 'S', 'E', 'E', 'S', 'W', 'N', 'W']
+    };
     expect(() => {
       validator.check(validSample);
     }).not.toThrow();
   });
 
-  test('invalid columns', () => {
+  test('invalid columns type', () => {
+    const invalidColumns = {
+      columns: '',
+      rows: 5,
+      position: { x: 1, y: 2 },
+      dirt: [],
+      drivingInstructions: []
+    };
     expect(() => {
       validator.check(invalidColumns);
     }).toThrow();
   });
 
-  test('invalid rows', () => {
+  test('invalid columns value', () => {
+    const invalidColumns = {
+      columns: -5,
+      rows: 5,
+      position: { x: 1, y: 2 },
+      dirt: [],
+      drivingInstructions: []
+    };
+    expect(() => {
+      validator.check(invalidColumns);
+    }).toThrow();
+  });
+
+  test('invalid rows type', () => {
+    const invalidRows = {
+      columns: 5,
+      rows: [],
+      position: { x: 1, y: 2 },
+      dirt: [],
+      drivingInstructions: []
+    };
     expect(() => {
       validator.check(invalidRows);
+    }).toThrow();
+  });
+
+  test('invalid rows value', () => {
+    const invalidRows = {
+      columns: 5,
+      rows: -5,
+      position: { x: 1, y: 2 },
+      dirt: [],
+      drivingInstructions: []
+    };
+    expect(() => {
+      validator.check(invalidRows);
+    }).toThrow();
+  });
+
+  test('invalid position format', () => {
+    const invalidPosition = {
+      columns: 5,
+      rows: 5,
+      position: { a: 1, b: 2 },
+      dirt: [],
+      drivingInstructions: []
+    };
+    expect(() => {
+      validator.check(invalidPosition);
+    }).toThrow();
+  });
+
+  test('invalid position types', () => {
+    const invalidPosition = {
+      columns: 5,
+      rows: 5,
+      position: { x: [], y: '2' },
+      dirt: [],
+      drivingInstructions: []
+    };
+    expect(() => {
+      validator.check(invalidPosition);
+    }).toThrow();
+  });
+
+  test('invalid position values', () => {
+    const invalidPosition = {
+      columns: 5,
+      rows: 5,
+      position: { x: -1, y: 6 },
+      dirt: [],
+      drivingInstructions: []
+    };
+    expect(() => {
+      validator.check(invalidPosition);
     }).toThrow();
   });
 });
--- src/validator.js
+++ src/validator.js
@@ -1,12 +1,37 @@
+const checkCoordinates = (name, value, maxX, maxY) => {
+  if (value['x'] === undefined || value['y'] === undefined) {
+    throw `Validation error, '${name}' must contain an 'x' and 'y' coordinate. Value passed was ${value}`;
+  }
+  checkWrongType(`${name}.x`, value.x, 'number');
+  checkWrongType(`${name}.y`, value.y, 'number');
+  checkNumberRange(`${name}.x`, value.x, 0, maxX);
+  checkNumberRange(`${name}.y`, value.y, 0, maxY);
+};
+
 const checkPositiveNumber = (name, value) => {
   if (typeof value === 'number' && value < 0) {
     throw `Validation error, '${name}' must be a positive number. Value passed was ${value}`;
   }
 };
 
+const checkNumberRange = (name, value, min, max) => {
+  if (value < min || value > max) {
+    throw `Validation error, '${name}' must be in the range from ${min} to ${max} inclusive. Value passed was ${value}`;
+  }
+};
+
+const checkWrongType = (name, value, type) => {
+  if (typeof value != type) {
+    throw `Validation error, '${name}' must be a ${type}.`;
+  }
+};
+
 export default class Validator {
   check(data) {
+    checkWrongType('columns', data.columns, 'number');
     checkPositiveNumber('columns', data.columns);
+    checkWrongType('rows', data.rows, 'number');
     checkPositiveNumber('rows', data.rows);
+    checkCoordinates('position', data.position, data.columns, data.rows);
   }
 }
